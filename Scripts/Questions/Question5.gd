84db6e855a523de09dcc67e78f2e967eea152aa4
Guillermo Vill <guillermVillalobos@hotmail.com>
2020-01-28 21:54:09 +0000

    quick fix

--- Scripts/Questions/Question5.gd
+++ Scripts/Questions/Question5.gd
@@ -3,4 +3,4 @@ extends "res://Scripts/Question.gd"
 func _ready():
 	load_question()
 	title = "Question 5"
-	dialogue = [[]]
+	dialogue = [["This question requires you to identify the output of the expression presented",1],[question,2]]
